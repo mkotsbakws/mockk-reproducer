383444c2e767d29383d2b32848315a9f628bb6ef
Urs Joss <urs.joss@gmx.ch>
2024-02-29 21:11:05 +0100

    Simple project setup with kotest, mockk-1.13.9 and kluent

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-framework-api:5.8.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("org.amshove.kluent:kluent:1.73")
+}
+
+tasks.test {
+    useJUnitPlatform {
+        includeEngines("kotest")
+    }
+}
+kotlin {
+    jvmToolchain(17)
+}
